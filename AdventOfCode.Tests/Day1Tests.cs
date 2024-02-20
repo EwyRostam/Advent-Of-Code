b3bf27eb0254b57bdb34888595a91914315dca90
Ewy Rostam <ewy.rost@gmail.com>
2024-02-20 19:49:25 +0100

    Setup base for project

---
+++ AdventOfCode.Tests/Day1Tests.cs
@@ -0,0 +1,14 @@
+namespace AdventOfCode.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CalibrationCalculator_Returns142_FromExampleDocument()
+    {
+        //ARRANGE
+
+        //ACT
+
+        //ASSERT
+    }
+}
